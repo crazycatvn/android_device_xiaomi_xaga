bd4fed9b0901401796cc26ba61c7d6d3055e4e97
Michael Bestas <mkbestas@lineageos.org>
2024-05-12 06:33:38 +0300

    xaga: Remove unused build description override
    Change-Id: I3d53f1cc5d4711403b53b723b95a175e6220702c

--- lineage_xaga.mk
+++ lineage_xaga.mk
@@ -34,6 +34,5 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="xaga-user 14 SP1A.210812.016 OS2.0.3.0.ULOCNXM release-keys" \
     BuildFingerprint=Redmi/xaga/xaga:12/SP1A.210812.016/OS2.0.3.0.ULOCNXM:user/release-keys \
     DeviceProduct=xaga
